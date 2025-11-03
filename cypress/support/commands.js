ef4215f5548b384788b7a7522cb0ac8953002e1f
Flávia Morais <152706350+flavia-orm@users.noreply.github.com>
2025-11-03 14:55:28 -0300

    Fix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 
 Cypress.Commands.add('login', () => {
-    cy.visit('https://www.saucedemo.com/') // substitua pela sua URL
+    cy.visit('https://www.saucedemo.com/') 
     cy.get('[data-test="username"]').type(Cypress.env('username'))
     cy.get('[data-test="password"]').type(Cypress.env('password'))
     cy.get('[data-test="login-button"]').click()
